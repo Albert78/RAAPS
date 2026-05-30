cfbb88d37e59d76e9ee18dec2ccfc93f207255a0
Daniel Höh <dhoeh78@gmail.com>
2026-05-30 08:33:21 +0200

    Add docs

--- app/src/sim/kotlin/de/dh/raaps/Injection.kt
+++ app/src/sim/kotlin/de/dh/raaps/Injection.kt
@@ -5,6 +5,12 @@ import de.dh.raaps.common.model.PluginManager
 import de.dh.raaps.core.aps.APS
 import de.dh.raaps.plugin.simbody.SimBodyPlugin
 
+/**
+ * Setup for a system where glucose source and insulin pump are provided by the [SimBodyPlugin],
+ * which simulates real situations like food intake, sports, illness, stress etc.
+ * With this plugin, we can interactively test our core calculation algorithms and the behavior
+ * of the app in simulated, "real" situations.
+ */
 fun setupSystem(aps: APS, pluginManager: PluginManager, application: Application) {
     val plugin = SimBodyPlugin(application)
     pluginManager.addPlugin(plugin)
