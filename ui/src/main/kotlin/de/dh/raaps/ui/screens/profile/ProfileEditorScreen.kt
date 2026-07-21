ebbebaf2cb09435f0f4a06e5ad481352a2ecb040
Daniel Höh <dhoeh78@gmail.com>
2026-07-21 16:33:03 +0200

    Let profile name edit field make the keyboard to suggest a capital letter at the first char

--- ui/src/main/kotlin/de/dh/raaps/ui/screens/profile/ProfileEditorScreen.kt
+++ ui/src/main/kotlin/de/dh/raaps/ui/screens/profile/ProfileEditorScreen.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -56,6 +57,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalLocale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -288,7 +290,8 @@ fun ProfileDetailEditor(
                     .fillMaxWidth()
                     .padding(16.dp),
                 singleLine = true,
-                isError = !isNameValid
+                isError = !isNameValid,
+                keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences)
             )
 
             SecondaryTabRow(selectedTabIndex = selectedTab) {
